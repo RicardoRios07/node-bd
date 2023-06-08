bee414dfb08b3d38c8e9b2c8db65832f48d8f738
RickDeuS <72361340+RickDeuS@users.noreply.github.com>
2023-06-08 18:41:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const estudiantesRouter = require('./routes/estudiantes');
-const docentesRouter = require('./routes/docentes');
-
+const mongoose = require('mongoose');
+const docentesRouter = require('./routes/docentesRoutes');
+const estudiantesRouter = require('./routes/estudiantesRoutes');
 const app = express();
 const port = 3000;
-const uri = 'mongodb://127.0.0.1:27017/bd-prueba';
-const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-client.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    return;
-  }
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Conexión exitosa a la base de datos');
+}).catch((error) => {
+  console.error('Error al conectar a la base de datos:', error);
+});
 
-  console.log('Conectado a la base de datos.');
-  const db = client.db();
+app.use(express.json());
+app.use('/docentes', docentesRouter);
+app.use('/estudiantes', estudiantesRouter);
 
-  app.use(express.json());
-
-  app.use('/estudiantes', estudiantesRouter(db));
-  app.use('/docentes', docentesRouter(db));
-
-  const server = app.listen(port, () => {
-    console.log(`Servidor en ejecución en http://localhost:${port}`);
-  });
-
-  server.on('error', (error) => {
-    console.error('Error al iniciar el servidor:', error);
-  });
-
-  setTimeout(() => {
-    if (!server.listening) {
-      console.error('Tiempo de espera agotado. No se pudo iniciar el servidor.');
-      process.exit(1);
-    }
-  }, 5000);
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
